15c4b775059575dc8ae456bdcdf36c63aac8a918
sunny-g <sunny.gonna@gmail.com>
2015-04-21 14:45:57 -0700

    edits edge limits

--- server/db/neoOperations.js
+++ server/db/neoOperations.js
@@ -6,32 +6,33 @@ Meteor.neo4j.methods({
   ex: return 'MATCH (a:Player {_id:{playerId}}) DELETE a'
     > this will match a doc in Player based on an obj with a prop
       playerId, then delete it
-
    */
 
 });
 
+// TODO: REFACTOR to use Meteor.N4JDB.query and opts
+
 var createUser = function(user, callback) {
   // saves:
-  // user.id and username
-  // stellar address and secret key
+  // user.id, username and stellar address
   var query = 'CREATE (:User {_id:"' + user._id +
     '",' + 'username:"' + user.username +
-    '",' + 'address:"' + user.profile.stellar.account_id + '"})';
+    '",' + 'address:"' + user.profile.stellar.account_id + 
+    '"})';
 
   //return neoQuerySync(query, null);
   neoQuery(query, null, callback);
 };
 
 var createEdge = function(sourceAddr, targetAddr, limit, callback) {
-  // handle simple trustSet (that is, create/overwrite)
-    // TODO: allow users to add or remove trustlines
 
-  // use MERGE to update/create trustline:
-    // http://neo4j.com/docs/stable/query-merge.html
   var query = 'MATCH (s {address:"' + sourceAddr +
     '"}),(t {address:"' + targetAddr + '"})' +
-    'CREATE (s)-[:TRUST {limit:' + limit + '}]->(t)';
+    'MERGE (s)-[limit:TRUST]->(t)' +
+    ' ON MATCH SET limit.limit = ' + limit +
+    ' ON CREATE SET limit.limit = ' + limit + 
+      ', limit.source = "' + sourceAddr +
+      '", limit.target = "' + targetAddr + '";';
 
   neoQuery(query, null, callback);
 };
