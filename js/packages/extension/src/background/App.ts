0cc5300aa49ce86afea32bd4e5bf9169b5a2fbc8
Sunny G <sunny.gonna@gmail.com>
2019-01-19 09:28:46 -0600

    removes unnecessary browser extension (for now)

--- js/packages/extension/src/background/App.ts
+++
@@ -1,9 +0,0 @@
-export default function main(sources: { ACTION: any }) {
-  const pong$ = sources.ACTION
-    .filter((action: { type: string }) => action.type === 'PING')
-    .constant({ type: 'PONG' });
-
-  return {
-    ACTION: pong$,
-  };
-}
--- js/packages/extension/src/content/index.ts
+++
@@ -1,3 +0,0 @@
-/* global window */
-
-console.log('hello world (from content script!)', window.chrome);
