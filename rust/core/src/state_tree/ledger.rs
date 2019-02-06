88e1e4f7c6b1b0ae0cebcbd11d314313ac841b3e
Sunny G <sunny.gonna@gmail.com>
2019-02-06 15:21:19 -0600

    split context and state_trees, move them

--- rust/core/src/state_tree/ledger.rs
+++ rust/core/src/state_tree/ledger.rs
@@ -1,61 +1,30 @@
-use std::collections::HashMap;
 use crate::{
     ledger::Ledger,
     operations::{
-        base::{
-            OperationContext as IOperationContext,
-            OperationEffects,
-        },
+        base::Context as IOperationContext,
+        context::OperationContext,
         LedgerOperation,
         Error as LedgerOperationError,
     },
 };
 
 /**
- * Stores the ledger state and any side effects of applying an operation
- */
-pub struct OperationContext {
-    sender: &'static str,
-    ledger: Ledger,
-    effects: OperationEffects,
-}
-
-impl OperationContext {
-    pub fn new(ledger: Ledger) -> Self {
-        OperationContext {
-            sender: "",
-            ledger,
-            effects: HashMap::new()
-        }
-    }
-}
-
-impl IOperationContext for OperationContext {
-    fn sender(&self) -> &str { &self.sender }
-    fn ledger(&self) -> &Ledger { &self.ledger }
-    fn effects(&self) -> &OperationEffects { &self.effects }
-
-    fn mut_ledger(&mut self) -> &mut Ledger { &mut self.ledger }
-    fn mut_effects(&mut self) -> &mut OperationEffects { &mut self.effects }
-}
-
-/**
  * TODO: move this to multiledger.rs?
  * Contains a list of `OperationContext`s, i.e. the entire potential history
  * of a single ledger.
  *
- * Since operations within HTL transactions can succeed or fail
+ * Because operations within HTL transactions can succeed or fail,
  * `OperationContext`s are either cloned or removed from the
- * `SingleLedgerContexts` before the newer operations are applied. This allows
+ * `LedgerStateTree` before the newer operations are applied. This allows
  * certain operations to be applied on top of currently unresolved operations.
  */
-pub struct SingleLedgerContexts(Vec<OperationContext>);
+pub struct LedgerStateTree(Vec<OperationContext>);
 
-impl SingleLedgerContexts {
+impl LedgerStateTree {
     pub fn from(ledger: Ledger) -> Self {
         let mut contexts = Vec::new();
         contexts.push(OperationContext::new(ledger));
-        SingleLedgerContexts(contexts)
+        LedgerStateTree(contexts)
     }
 
     pub fn current_seq_no(&self) -> Option<u64> {
