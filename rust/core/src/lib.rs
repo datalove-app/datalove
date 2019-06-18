18cb1664a16681bddc569bb9b217747c507dea94
Sunny G <sunny.gonna@gmail.com>
2019-06-18 13:58:09 -0700

    remove unnecessary rust crates

--- rust/core/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//!
-
-#![feature(try_from)]
-
-pub mod types;
-pub mod ledger;
-pub mod operations;
-pub mod transactions;
-pub mod state_tree;
--- rust/core/src/types.rs
+++
@@ -1,6 +0,0 @@
-use std::rc::Rc;
-use holochain_core_types::hash::HashString;
-
-pub type AgentAddress = HashString;
-pub type AgentAddressRc = Rc<AgentAddress>;
-pub type HashStringRc = Rc<HashString>;
