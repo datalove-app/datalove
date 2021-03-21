266883ba7b7c39418c374ea7b1fec83187df0b69
sunny-g <sunny.gonna@gmail.com>
2021-03-21 12:12:25 -0500

    more bleh

--- crates/persona/src/interfaces.rs
+++
@@ -1,7 +0,0 @@
-use std::{fmt::Debug, ops::AddAssign};
-
-///
-pub(crate) trait Timestamp: Clone + Debug + Default + Eq + AddAssign {}
-
-///
-pub(crate) trait TimestampVar<T: Timestamp, F: Field>: AllocVar<T, F> {}
--- crates/time/src/v0/mod.rs
+++
@@ -1,5 +0,0 @@
-mod value;
-mod var;
-
-pub use value::BloomClock;
-pub use var::BloomClockVar;
