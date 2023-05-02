05885acc0728c5e994b688e500dc976563441b2c
sunny-g <sunny.gonna@gmail.com>
2023-05-02 16:48:22 -0500

    w/e

--- flake.nix
+++ flake.nix
@@ -2,11 +2,32 @@
   description = "Datalove";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    lean.url = "github:leanprover/lean4";
-    lake.url = "github:leanprover/lake";
-    # std4.url = "github:leanprover/std4";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    lean4 = {
+      # url = "github:HamelinDavid/lean4";
+      url = "github:leanprover/lean4";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+
+    lake = {
+      url = "github:leanprover/lake";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+        lean.follows = "lean4";
+      };
+    };
+
+    # std4 = {
+    #   # url = "github:leanprover/std4";
+    #   url = "./vendor/std4";
+    #   flake = false;
+    # };
 
     # lightdata.lean.url = "github:lurk-lab/lightdata.lean";
     # lurk.lean.url = "github:lurk-lab/lurk.lean";
@@ -17,29 +38,42 @@
     # typst.url = "github:typst/typst";
   };
 
-  outputs = { self, nixpkgs, lean, lake, flake-utils }:
-    let
-      supportedSystems = [
-        # "aarch64-linux"
-        "aarch64-darwin"
-        "i686-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-    in
-    flake-utils.lib.eachSystem supportedSystems (system:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    lean4,
+    lake,
+    # std4,
+    ...
+  }:
+    # let
+    #   supportedSystems = [
+    #     # "aarch64-linux"
+    #     "aarch64-darwin"
+    #     "i686-linux"
+    #     "x86_64-darwin"
+    #     "x86_64-linux"
+    #   ];
+    # in
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        packageName = "Datalove";
-        leanPkgs = lean.packages.${system};
-        lakePkgs = lake.packages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          # config.allowUnfree = true;
+        };
+        leanPkgs = lean4.packages.${system};
+        lakePkg = lake.outputs.defaultPackage.${system};
+
         # deps
         # lurk = leanPkgs.buildLeanPackage {
         #   name = "Lurk";
         #   src = ./vendor/lurk-lean;
         # };
-        # std4 = leanPkgs.buildLeanPackage {
+        # Std = leanPkgs.buildLeanPackage {
         #   name = "Std";
-        #   src = ./vendor/std4/std;
+        #   # roots = ["Std"];
+        #   src = ./vendor/std4/.;
         # };
         # yatima = leanPkgs.buildLeanPackage {
         #   name = "Yatima";
@@ -48,27 +82,27 @@
         # };
 
         pkg = leanPkgs.buildLeanPackage {
-          name = packageName;
+          name = "Datalove";
+          roots = ["Datalove"];
           src = ./.;
-          # deps = [ lean.Lean std4 ];
+          deps = with leanPkgs; [
+            Init Lean
+          ];
         };
       in {
+        devShell = pkgs.mkShell rec {
+          buildInputs = with pkgs; [ leanPkgs.lean-all lakePkg ];
+          shellHook = ''
+            export PATH=${pkgs.lib.makeBinPath buildInputs}:$PATH
+          '';
+          # LEAN_PATH = "./src:./test";
+          # LEAN_SRC_PATH = "./src:./test";
+        };
+
         packages = pkg // {
-          # ${name} = datalove.sharedLib;
-          # inherit (datalove) lean-package print-paths;
-          inherit (leanPkgs) lean-all;
-          inherit (lakePkgs) lake;
+          inherit (leanPkgs) lean;
         };
 
         defaultPackage = pkg.modRoot;
-        # defaultPackage = self.packages.${system}.datalove;
-        # devShell = pkg.mkShell {
-        #   buildInputs = with pkg; [
-        #     leanPkgs.lean-all
-        #     lakePkgs.lake
-        #   ];
-        #   LEAN_PATH = "./src:./test";
-        #   LEAN_SRC_PATH = "./src:./test";
-        # };
       });
 }
